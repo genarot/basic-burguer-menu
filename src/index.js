518c72e270209fb05733aa5d81eca5494ad5d9fb
gjtinoco <genaro_13@hotmail.es>
2018-11-12 16:56:06 -0600

    Basic redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import  {BrowserRouter} from 'react-router-dom'
 
 //Redux
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './store/reducer'
 
@@ -11,8 +11,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const logger = (store) => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action)
+            console.log('[Middleware] next state', store.getState())
+            return result;
+        }
+    }
+}
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 
 ReactDOM.render(
     <Provider store={store}>
